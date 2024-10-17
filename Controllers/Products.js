630c667049f4874ff176cb06bceeb7f40a54cf17
haroon909 <aharoon909@gmail.com>
2024-10-18 03:30:16 +0500

    --- Auto Git Commit ---

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -6,11 +6,12 @@ const Get_Product_view = (req, res) => {
 //@method Post
 //http://localhost:5000/
 const Post_Product_view = (req, res) => {
-    const {Product_Name, Product_Desc, Product_Price, Product_Cat} = req.body;
-    const Validator_name = /^[A-Za-z]{3,}$/;
-    const Validator_desc = /^(?=.*[A-Za-z]{3,})([A-Za-z0-9\s\W]{1,})(?<=\b(?:\S+\b\s*){0,100})$/;
-const Validator_price = /^\$[0-9]{1,6}$/ ;
-const Validator_cata = //
+  const { Product_Name, Product_Desc, Product_Price, Product_Cat } = req.body;
+  const Validator_name = /^[A-Za-z]{3,}$/;
+  const Validator_desc =
+    /^(?=.*[A-Za-z]{3,})([A-Za-z0-9\s\W]{1,})(?<=\b(?:\S+\b\s*){0,100})$/;
+  const Validator_price = /^\$[0-9]{1,6}$/;
+  const Validator_cata = /^[A-Za-z]+(_[A-Za-z]+)*$/;
   return res.render("Products");
 };
 
