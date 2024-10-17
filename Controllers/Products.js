ba36ce1e299f1d36be5ae4d59fc9705e7c778060
haroon909 <aharoon909@gmail.com>
2024-10-18 03:40:51 +0500

    --- Auto Git Commit ---

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -6,7 +6,7 @@ const Get_Product_view = (req, res) => {
 //@method Post
 //http://localhost:5000/
 const Post_Product_view = (req, res) => {
-  const { Product_Name, Product_Desc, Product_Price, Product_Cat } = req.body;
+  const { Product_Name, Product_Desc, Product_Price, Product_Cata } = req.body;
   const Validator_name = /^(?=.{1,})[A-Za-z]+(_[A-Za-z]+)*$/;
   const Validator_desc =
     /^(?=.{1,})(?=.*[A-Za-z]{3,})([A-Za-z0-9\s\W]{1,})(?<=\b(?:\S+\b\s*){0,100})$/;
@@ -14,9 +14,15 @@ const Post_Product_view = (req, res) => {
   const Validator_cata = /^(?=.{1,})[A-Za-z]+(_[A-Za-z]+)*$/;
 
   const Validated_name = Validator_name.test(Product_Name);
-  const Validated_decs = Validator_name.test(Product_Name);
-  const Validated_ = Validator_name.test(Product_Name);
-  const Validated_name = Validator_name.test(Product_Name);
+  const Validated_decs = Validator_desc.test(Product_Desc);
+  const Validated_price = Validator_price.test(Product_Price);
+  const Validated_cata = Validator_cata.test(Product_Cata);
+
+  if(Validated_name == true){
+
+  }else{
+    
+  }
   
   return res.render("Products");
 };
