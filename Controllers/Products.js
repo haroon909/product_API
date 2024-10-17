c6062c7c72324458c15b494b3b19db97f2c51338
haroon909 <aharoon909@gmail.com>
2024-10-18 03:54:57 +0500

    --- Auto Git Commit ---

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -20,22 +20,29 @@ const Post_Product_view = (req, res) => {
 
   if (Validated_name == true) {
     if (Validated_decs == true) {
-        if(Validated_price == true){
-            if(Validated_cata == true){
-                const New_Product = {
-                    
-
-                }
-
-            }else{
-                return res.send(`Product category cannot be null or empty. Product category must contain only letters and underscores ('_') only for spacing, with no numbers or special characters, and must be at least 3 characters long.`)
-            }
-
-        }else{
-            return res.send(`Product price cannot be null or empty. Product price must start with '$' and be a whole number between 1 and 6 digits, without spaces or special characters.`)
+      if (Validated_price == true) {
+        if (Validated_cata == true) {
+          const New_Product = {
+            Product_Name: Product_Name,
+            Product_Desc: Product_Desc,
+            Product_Price: Product_Price,
+            Product_Cata: Product_Cata,
+          };
+          console.log(New_Product);
+        } else {
+          return res.send(
+            `Product category cannot be null or empty. Product category must contain only letters and underscores ('_') only for spacing, with no numbers or special characters, and must be at least 3 characters long.`
+          );
         }
-    }else{
-        return res.send(`Product description cannot be null or empty, product description must contain at least 3 letters, can include numbers and special characters, and cannot exceed 100 words.`)
+      } else {
+        return res.send(
+          `Product price cannot be null or empty. Product price must start with '$' and be a whole number between 1 and 6 digits, without spaces or special characters.`
+        );
+      }
+    } else {
+      return res.send(
+        `Product description cannot be null or empty, product description must contain at least 3 letters, can include numbers and special characters, and cannot exceed 100 words.`
+      );
     }
   } else {
     return res.send(
