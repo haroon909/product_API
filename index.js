367038c3cf1f4088309ee51ab2e3093fd400aac2
haroon909 <aharoon909@gmail.com>
2024-10-18 02:07:20 +0500

    --- Auto Git Commit ---

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const Express = require('express');
 const app = Express();
 const dotenv = require("dotenv").config();
-app.set('')
+app.set('view engine','ejs');
+app.use(Express.json());
+
+
+
+
+
+
+app.listen(process.env.Port, function(){
+    console.log(`Server is running On `)
+})
