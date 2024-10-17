66ae7e09ed305cc03cb697f2984ec17a4d45af23
haroon909 <aharoon909@gmail.com>
2024-10-18 03:07:19 +0500

    --- Auto Git Commit ---

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -6,7 +6,7 @@ const Get_Product_view = (req, res) => {
 //@method Post
 //http://localhost:5000/
 const Post_Product_view = (req, res) => {
-    const {} = req.body;
+    const {Product_Name, Product_Desc, Product_Price, Product_Cat} = req.body;
   return res.render("Products");
 };
 
