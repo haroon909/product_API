82aba682613eff09f31b6033dc2664b1496b6cfc
haroon909 <aharoon909@gmail.com>
2024-10-18 03:03:48 +0500

    --- Auto Git Commit ---

--- Controllers/Products.js
+++ Controllers/Products.js
@@ -1,15 +1,12 @@
 //@method Get
 //http://localhost:5000/
-const Get_Product_view = (req, res)=>{
-    return res.render('Products')
+const Get_Product_view = (req, res) => {
+  return res.render("Products");
 };
-const Post_Product_view = (req, res)=>{
-    return res.render('Products')
+//@method Post
+//http://localhost:5000/
+const Post_Product_view = (req, res) => {
+  return res.render("Products");
 };
 
-
-
-
-
-
-module.exports={Get_Product_view, Post_Product_view};
+module.exports = { Get_Product_view, Post_Product_view };
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const Express = require('express');
+const Express = require("express");
 const app = Express();
 const dotenv = require("dotenv").config();
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 app.use(Express.json());
-const {Get_Product_view}=require('./Controllers/Products');
-app.route("/").get(Get_Product_view).post(PostUser_Reg);
+const {
+  Get_Product_view,
+  Post_Product_view,
+} = require("./Controllers/Products");
+app.route("/").get(Get_Product_view).post(Post_Product_view);
 
-
-
-
-
-app.listen(process.env.Port, function(){
-    console.log(`Server is running On Port: ${process.env.Port}`)
-})
+app.listen(process.env.Port, function () {
+  console.log(`Server is running On Port: ${process.env.Port}`);
+});
